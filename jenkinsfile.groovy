0d27b3ad2550cb344a6cb73a92350cd0fc417303
MariosGramm <mariosgramm21@gmail.com>
2024-12-08 22:12:34 +0200

    Add files via upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,55 +1,49 @@
 pipeline {
     parameters {
-        booleanParam(name:'autoApprove', defaultValue:false, description:'Automatically run apply after terraform plan?')
+        booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
     }
     environment {
-        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-        AWS_SECRET_KEY    = credentials('AWS_SECRET_KEY')
+        AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
     }
-
     agent any
-        stages {
-            stage('checkout'){          //Terraform code retrieval 
-                steps{
-                    script {
-                        dir('Terraform')
-                        {
-                             git branch: 'main', url: "https://github.com/MariosGramm/MyFirstPipeline.git"
-                        }
-                    }
-                }
-            }
-
-            stage('InitPlan'){           //Terraform init + plan + tfplan.txt file
-                steps{
-                    bat 'pwd; cd terraform/ ; terraform init'
-                    bat 'pwd; cd terraform/ ; terraform plan - out tfplan'
-                    bat 'pwd; cd terraform/ ; terraform show -no-color tfplan > tfplan.txt'
-                }
-
-            }
-        
-           stage('Approval'){           //Αν ο χρήστης δεν έχει επιλέξει autoApprove
-                when {
-                    not {
-                        equals expected:true,actual:params.autoApprove
-                    }
-                }
-
-                steps{
-                    script {
-                        def plan = readFile 'terraform/tfplan.txt'
-                        input message : 'Do you want to apply the plan?'
-                        parameters : [text(name:'Plan', description: 'Please review the plan' , defaultValue: plan)]
-                    }
-                }
-            }
-
-            stage('Apply'){         //Terraform apply  
-                steps{
-                    bat 'pwd;cd terraform/ ; terraform apply -input=false tfplan'
+    stages {
+        stage('Checkout') {                 //Terraform code retrieval 
+            steps {
+                dir("terraform") {
+                    git branch: 'main', url: "https://github.com/MariosGramm/MyFirstPipeline.git"
                 }
             }
         }
+        stage('InitPlan') {                //Terraform init + plan + tfplan.txt file
+            steps {
+                dir('terraform') {
+                    bat 'terraform init'
+                    bat 'terraform plan -out=tfplan'
+                    bat 'terraform show -no-color tfplan > tfplan.txt'
+                }
+            }
+        }
+        stage('Approval') {              //Αν ο χρήστης δεν έχει επιλέξει autoApprove
+            when {
+                not {
+                    equals expected: true, actual: params.autoApprove
+                }
+            }
+            steps {
+                script {
+                    def plan = readFile 'terraform/tfplan.txt'
+                    input message: "Do you want to apply the plan?",
+                    parameters: [text(name: 'Plan', description: 'Please review the plan', defaultValue: plan)]
+                }
+            }
+        }
+        stage('Apply') {                //Terraform apply
+            steps {
+                dir('terraform') {
+                    bat 'terraform apply -input=false tfplan'
+                }
+            }
+        }
+    }
 }
-
